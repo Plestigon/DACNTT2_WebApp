029cfd357b8e7e7ee6a743c17e3e85352ff32ef1
DestinyError <lydangminh2706@gmail.com>
2023-12-17 12:05:36 +0700

    small url upd

--- demo/src/main/java/da2/webapp/demo/HomeController.java
+++ demo/src/main/java/da2/webapp/demo/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RequestMapping(path="/")
 public class HomeController {
     private final HomeService homeService;
 
