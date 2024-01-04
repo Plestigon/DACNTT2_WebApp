9017fbac015bc6c2ce8a6b08296086547c6ad71f
DestinyError <lydangminh2706@gmail.com>
2024-01-04 11:31:26 +0700

    temp remove login

--- main/src/main/java/tdtu_ems/main/MainApplication.java
+++ main/src/main/java/tdtu_ems/main/MainApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication()
--- main/src/main/java/tdtu_ems/main/controllers/HomeController.java
+++ main/src/main/java/tdtu_ems/main/controllers/HomeController.java
@@ -1,13 +1,13 @@
 package tdtu_ems.main.controllers;
 
-import tdtu_ems.main.models.SecurityUser;
+//import tdtu_ems.main.models.SecurityUser;
 import tdtu_ems.main.services.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +18,7 @@ import java.util.Collection;
 import java.util.List;
 
 //@EnableMethodSecurity
-@EnableMethodSecurity
+//@EnableMethodSecurity
 @Controller
 @RequestMapping(path="/")
 public class HomeController {
@@ -31,44 +31,44 @@ public class HomeController {
 
     @GetMapping("/")
     public String index(Model model) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String email = auth.getName();
-        Collection<GrantedAuthority> authorities = (Collection<GrantedAuthority>) auth.getAuthorities();
-        List<GrantedAuthority> roles = new ArrayList<GrantedAuthority>(authorities);
-        String role = roles.get(0).toString();
-        if (!role.equals("ROLE_ANONYMOUS")) {
-            SecurityUser u = (SecurityUser) auth.getPrincipal();
-            model.addAttribute("email", u.getUsername());
-            model.addAttribute("name", u.getName());
-            model.addAttribute("role", u.getRole());
-            return switch (u.getRole()) {
-                case "MANAGER" -> "Manager_page";
-                case "EMPLOYEE" -> "Employee_page";
-                default -> "redirect:/error";
-            };
-        }
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        String email = auth.getName();
+//        Collection<GrantedAuthority> authorities = (Collection<GrantedAuthority>) auth.getAuthorities();
+//        List<GrantedAuthority> roles = new ArrayList<GrantedAuthority>(authorities);
+//        String role = roles.get(0).toString();
+//        if (!role.equals("ROLE_ANONYMOUS")) {
+//            SecurityUser u = (SecurityUser) auth.getPrincipal();
+//            model.addAttribute("email", u.getUsername());
+//            model.addAttribute("name", u.getName());
+//            model.addAttribute("role", u.getRole());
+//            return switch (u.getRole()) {
+//                case "MANAGER" -> "Manager_page";
+//                case "EMPLOYEE" -> "Employee_page";
+//                default -> "redirect:/error";
+//            };
+//        }
         return "Landing_page";
     }
 
-    @PreAuthorize("hasRole('MANAGER')")
-    @GetMapping("/finances")
-    public String indexManager() {
-        return "Finances";
-    }
-
-    @PreAuthorize("hasRole('EMPLOYEE')")
-    @GetMapping("/complaints")
-    public String indexEmployee() {
-        return "Complaints_Form";
-    }
+//    @PreAuthorize("hasRole('MANAGER')")
+//    @GetMapping("/finances")
+//    public String indexManager() {
+//        return "Finances";
+//    }
+//
+//    @PreAuthorize("hasRole('EMPLOYEE')")
+//    @GetMapping("/complaints")
+//    public String indexEmployee() {
+//        return "Complaints_Form";
+//    }
 
     @GetMapping("/profile")
     public String profile(Model model) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        SecurityUser u = (SecurityUser) auth.getPrincipal();
-        model.addAttribute("email", u.getUsername());
-        model.addAttribute("name", u.getName());
-        model.addAttribute("role", u.getRole());
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        SecurityUser u = (SecurityUser) auth.getPrincipal();
+//        model.addAttribute("email", u.getUsername());
+//        model.addAttribute("name", u.getName());
+//        model.addAttribute("role", u.getRole());
         return "Profile_Page";
     }
 }
